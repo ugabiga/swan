6707dc6ecc6100fd510acda645e3311d57124afc
ugabiga <ugabiga@gmail.com>
2024-08-17 22:03:29 +0900

    Refactor code format

--- starter/internal/config/env.go
+++ starter/internal/config/env.go
@@ -3,9 +3,8 @@ package config
 import "github.com/ugabiga/swan/core"
 
 type EnvironmentVariables struct {
-	AppName string `validate:"required" env:"APP_NAME" json:"app_name,omitempty"`
-	Addr    string `validate:"required" env:"ADDR" json:"addr,omitempty"`
-
+	AppName        string `validate:"required" env:"APP_NAME" json:"app_name,omitempty"`
+	Addr           string `validate:"required" env:"ADDR" json:"addr,omitempty"`
 	EventDriver    string `validate:"required" env:"EVENT_DRIVER" json:"event_driver,omitempty"`
 	EventRedisAddr string `env:"EVENT_REDIS_ADDR" json:"event_redis_addr,omitempty"`
 	EventRedisDB   int    `env:"EVENT_REDIS_DB" json:"event_redis_db,omitempty"`
