202906fff6ce403c145b0830121d7b233379ef04
ugabiga <ugabiga@gmail.com>
2024-09-27 21:31:24 +0900

    refactor: domain to handler 2

--- cli/internal/commands/make_domain.go
+++ cli/internal/commands/make_domain.go
@@ -8,25 +8,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var MakeDomainCmd = &cobra.Command{
-	Use:   "make:domain",
+var MakeHandlerCommand = &cobra.Command{
+	Use:   "make:handler",
 	Short: "Create a new domain",
 	Run: func(cmd *cobra.Command, args []string) {
 		var (
-			domainName  string
+			handlerName string
 			routePrefix string
 		)
 
-		if err := huh.NewInput().Title("Domain Name").Value(&domainName).Run(); err != nil {
+		if err := huh.NewInput().
+			Title("Handler Name").
+			Value(&handlerName).
+			Run(); err != nil {
 			panic(err)
 		}
 
-		if err := huh.NewInput().Title("Route Prefix(eg: /api)").Value(&routePrefix).Run(); err != nil {
+		if err := huh.NewInput().
+			Title("Route Prefix(eg: /api)").
+			Value(&routePrefix).
+			Run(); err != nil {
 			panic(err)
 		}
 
-		generating.CreateDomain(domainName, routePrefix)
+		generating.CreateHandler(handlerName, routePrefix)
 
-		fmt.Printf("Domain %s created successfully\n", domainName)
+		fmt.Printf("Domain %s created successfully\n", handlerName)
 	},
 }
