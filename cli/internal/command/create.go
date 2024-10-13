03a781b39aeea615c09ce435839a4129885bce2c
ugabiga <ugabiga@gmail.com>
2024-10-14 00:29:16 +0900

    cli: fix command and event, add general struct

--- cli/internal/command/create.go
+++ cli/internal/command/create.go
@@ -15,10 +15,11 @@ import (
 )
 
 type TplData struct {
-	PackageName      string
-	StructOrFuncName string
-	StructType       string
-	HandlerURL       string
+	PackageName string
+	FuncName    string
+	StructType  string
+	HandlerURL  string // for handler
+	StructName  string
 }
 
 var CreateCmd = &cobra.Command{
@@ -28,73 +29,152 @@ var CreateCmd = &cobra.Command{
 	},
 }
 
-var CreateCmdCommand = &cobra.Command{
-	Use:   "command",
-	Short: "create a new command",
-	Args:  cobra.MaximumNArgs(2),
-	Run: func(cmd *cobra.Command, args []string) {
-		path := args[0]
-		genType := cmd.Use
-		tplData := TplData{}
-		if err := gen(tplData, genType, path); err != nil {
-			fmt.Printf("Error while creating struct: %s", err)
-			return
-		}
-	},
-}
-
 var CreateCmdHandler = &cobra.Command{
 	Use:   "handler",
 	Short: "create a new handler",
 	Long:  "create handler users /api",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Printf("args: %v", args)
 		path := args[0]
 		genType := cmd.Use
 		tplData := TplData{
 			HandlerURL: args[1] + "/" + path,
 		}
-		if err := gen(tplData, genType, path); err != nil {
-			fmt.Printf("Error while creating struct: %s", err)
+		if err := gen(genType, tplData, path); err != nil {
+			log.Printf("Error while creating: %s", err)
+			return
+		}
+	},
+}
+
+var CreateCmdCommand = &cobra.Command{
+	Use:   "command",
+	Short: "create a new command",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		path := args[0]
+		genType := cmd.Use
+		tplData := TplData{}
+		if err := gen(genType, tplData, path); err != nil {
+			log.Printf("Error while creating: %s", err)
+			return
+		}
+	},
+}
+
+var CreateCmdEvent = &cobra.Command{
+	Use:   "event",
+	Short: "create a new event",
+	Long:  "Usage : create event users",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		path := args[0]
+		genType := cmd.Use
+		tplData := TplData{}
+		if err := gen(genType, tplData, path); err != nil {
+			log.Printf("Error while creating: %s", err)
+			return
+		}
+	},
+}
+
+var CreateCmdStruct = &cobra.Command{
+	Use:   "struct",
+	Short: "create a new struct",
+	Long:  "Usage : create struct users Service",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		path := args[0]
+		genType := cmd.Use
+		tplData := TplData{
+			StructName: args[1],
+		}
+		if err := gen(genType, tplData, path); err != nil {
+			log.Printf("Error while creating: %s", err)
 			return
 		}
 	},
 }
 
 func CreateCmds() *cobra.Command {
-	CreateCmd.AddCommand(CreateCmdCommand)
 	CreateCmd.AddCommand(CreateCmdHandler)
+	CreateCmd.AddCommand(CreateCmdCommand)
+	CreateCmd.AddCommand(CreateCmdEvent)
+	CreateCmd.AddCommand(CreateCmdStruct)
 	return CreateCmd
 }
 
-func gen(tplData TplData, genType, path string) error {
+func invokerPathByType(genType string) string {
+	switch genType {
+	case "command":
+		return generate.CommandPath
+	case "event":
+		return generate.EventPath
+	default:
+		return generate.AppPath
+	}
+}
+
+func registerFuncByType(genType string) string {
+	switch genType {
+	case "command", "event":
+		return "app.RegisterInvokers("
+	default:
+		return "app.RegisterProviders("
+	}
+}
+
+func funcNameByType(genType string, tplData TplData) string {
+	switch genType {
+	case "command":
+		return "SetCommands"
+	case "event":
+		return "SetEvents"
+	case "handler":
+		return "NewHandler"
+	case "struct":
+		return "New" + tplData.StructName
+	default:
+		return ""
+	}
+}
+
+func fileNameByType(genType string, tplData TplData) string {
+	switch genType {
+	case "struct":
+		return strings.ToLower(tplData.StructName) + ".go"
+	default:
+		return strings.ToLower(genType) + ".go"
+	}
+}
+
+func gen(genType string, tplData TplData, path string) error {
 	filePath := fmt.Sprintf("internal/%s/", path)
-	tplData.StructOrFuncName = structOrFuncNameByType(genType)
+	tplData.FuncName = funcNameByType(genType, tplData)
 	tplData.PackageName = extractPackageName(filePath)
 
 	if err := createTemplate(filePath, genType, tplData); err != nil {
 		return err
 	}
 
-	if err := registerInvokerOrProvider(generate.CommandPath, tplData.PackageName, tplData.StructOrFuncName); err != nil {
+	if err := registerInvokerOrProvider(genType, tplData.PackageName, tplData.FuncName); err != nil {
 		return err
 	}
 
-	log.Printf("Generated %s%s", filePath, strings.ToLower(genType)+".go")
+	log.Printf("Generated %s%s", filePath, fileNameByType(genType, tplData))
 
 	return nil
 }
 
-func createTemplate(filePath, generationType string, tplData TplData) error {
-	f := createFile(filePath, strings.ToLower(generationType)+".go")
+func createTemplate(filePath, genType string, tplData TplData) error {
+	f := createFile(filePath, fileNameByType(genType, tplData))
 	if f == nil {
-		log.Printf("warn: file %s%s %s", filePath, strings.ToLower(generationType)+".go", "already exists.")
+		log.Printf("warn: file %s%s %s", filePath, strings.ToLower(genType)+".go", "already exists.")
 		return nil
 	}
 	defer f.Close()
 
-	t, err := template.ParseFS(tpl.CreateTemplateFS, "create/"+generationType+".tmpl")
+	t, err := template.ParseFS(tpl.CreateTemplateFS, "create/"+genType+".tmpl")
 	if err != nil {
 		log.Fatalf("Failed to parse template: %v", err)
 		return err
@@ -153,6 +233,13 @@ func registerInvokerOrProvider(genType, packageName, structName string) error {
 	return nil
 }
 
+func extractPackageName(path string) string {
+	packageName := filepath.Base(path)
+	packageName = strings.TrimSuffix(packageName, "/")
+
+	return packageName
+}
+
 func readFileContent(filePath string) (string, error) {
 	bytes, err := os.ReadFile(filePath)
 	if err != nil {
@@ -162,13 +249,6 @@ func readFileContent(filePath string) (string, error) {
 	fileContent := string(bytes)
 
 	return fileContent, nil
-}
-
-func extractPackageName(path string) string {
-	packageName := filepath.Base(path)
-	packageName = strings.TrimSuffix(packageName, "/")
-
-	return packageName
 }
 
 func createFile(dirPath string, filename string) *os.File {
@@ -187,33 +267,4 @@ func createFile(dirPath string, filename string) *os.File {
 	}
 
 	return file
-}
-
-func invokerPathByType(genType string) string {
-	switch genType {
-	case "command":
-		return generate.CommandPath
-	default:
-		return generate.AppPath
-	}
-}
-
-func registerFuncByType(genType string) string {
-	switch genType {
-	case "command":
-		return "app.RegisterInvokers("
-	default:
-		return "app.RegisterProviders("
-	}
-}
-
-func structOrFuncNameByType(genType string) string {
-	switch genType {
-	case "command":
-		return "SetCommands"
-	case "handler":
-		return "NewHandler"
-	default:
-		return ""
-	}
 }
