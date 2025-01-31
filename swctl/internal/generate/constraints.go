472bbe867f671bc37da2cd65d8679f63fe59b384
ugabiga <ugabiga@gmail.com>
2025-01-31 18:02:44 +0900

    refactor: consolidate constants in constraints.go

--- swctl/internal/generate/constraints.go
+++ swctl/internal/generate/constraints.go
@@ -4,13 +4,10 @@ const (
 	RepoAddr        = "https://github.com/ugabiga/swan.git"
 	BoostrapDirName = "bootstrap"
 	BootstrapPath   = "swan/bootstrap"
-)
-
-const (
-	AppRootPath   = "./internal/app"
-	AppPath       = AppRootPath + "/app.go"
-	ContainerPath = AppRootPath + "/container.go"
-	EventPath     = AppRootPath + "/event.go"
-	CommandPath   = AppRootPath + "/commands.go"
-	RouterPath    = AppRootPath + "/server/router.go"
+	AppRootPath     = "./internal/app"
+	AppPath         = AppRootPath + "/app.go"
+	ContainerPath   = AppRootPath + "/container.go"
+	EventPath       = AppRootPath + "/event.go"
+	CommandPath     = AppRootPath + "/commands.go"
+	RouterPath      = AppRootPath + "/server/router.go"
 )
